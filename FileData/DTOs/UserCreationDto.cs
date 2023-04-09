2272db0e782759960f20dbbab0f14ab25103b61f
Oriana11 <rubyjpy@gmail.com>
2023-04-09 16:38:14 +0200

    Todo tutorial

---
+++ FileData/DTOs/UserCreationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs
+{
+    public class UserCreationDto
+    {
+        public string UserName { get; }
+
+        public UserCreationDto(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ FileData/Models/Todo.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public User Owner { get;}
+
+        public string Title { get;}
+        public bool IsCompleted { get;}
+
+        public Todo(User owner, string title)
+        {
+            owner = owner;
+            Title = title;
+        }
+    }
+
+   
+}
